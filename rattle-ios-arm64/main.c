486da6b3dd9853c9da1572686a310140bf921f0a
Jason Kim <nosajmik@umich.edu>
2022-08-02 17:52:40 -0400

    Initial Commit

---
+++ rattle-ios-arm64/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  rattle-ios-arm64
+//
+//  Created by Test on 8/2/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
